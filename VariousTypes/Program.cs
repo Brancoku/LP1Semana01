d0c4aabbae4811381d1416c3dd4dfd3b5f4a43ce
Gabriela Branco <lindosogb@gmail.com>
2024-02-22 23:47:10 +0000

    add new int variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a = 1;
+            short b = 2;
+            int c = 4;
+            long d = 8L;
+            ulong e = 16UL;
+            uint f = 32U;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+        }
+    }
+}

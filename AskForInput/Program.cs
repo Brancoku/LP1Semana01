8c6a9b571a42de1a682ebecb5bf8617fad3b9d69
Gabriela Branco <lindosogb@gmail.com>
2024-02-23 00:54:12 +0000

    add new user input

---
+++ AskForInput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a int number:");
+            string input = Console.ReadLine();
+            int.TryParse(input, out int intValue);
+
+            Console.WriteLine("Give me a real number:");
+            string input2 = Console.ReadLine();
+            float.TryParse(input2, out float floatValue);
+            
+            float soma = intValue + floatValue;
+            Console.WriteLine($"{input} + {input2} = {soma}");
+        }
+    }
+}
